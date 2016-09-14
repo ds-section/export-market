229c6782a130c33b76fbea7b858f78ac613ed44a
Shih-Po <spurcheng@gmail.com>
2016-09-14 15:40:07 +0800

    add .gitignore and transfer_data.py

---
+++ transfer_data.py
@@ -0,0 +1,68 @@
+import pandas as pd
+
+# read every row and transfer to string that split by comma and double quote
+def formalize_row(row):
+    
+    rs = ''
+
+    for col in row.split('|'):
+        rs += ('\"' + col.strip() + '\",')
+
+    # Change the last comma to \n
+    rs = rs[:-1].upper() + '\n'
+
+    return(rs)
+
+# input txt from export goods than output accepted csv to sql 
+def formalize_to_well_form_csv(txt_path):
+    
+    f_list = []
+    csv_path = txt_path.split('.')[0] + '.csv'
+
+    with open(txt_path, 'r') as f:
+
+        for line in f:
+            f_list.append(formalize_row(line))
+
+    with open(csv_path, 'w') as f:
+
+        for row in f_list:
+            f.write(row)
+    
+    print('formalize_to_well_form_csv(' + txt_path + ') finished!')
+
+
+# use pandas to clean the comma out in weight col, add _id
+# input: csv, output: the same csv 
+def parse_col(csv_path):
+    
+    csv = pd.read_csv(csv_path)
+
+    # set the col to str type (replacable)
+    csv['數量'] = csv['數量'].astype(str)
+    csv['重量'] = csv['重量'].astype(str)
+    csv['價值'] = csv['價值'].astype(str)
+
+    csv['數量'] = csv['數量'].str.replace(pat=',', repl='')
+    csv['重量'] = csv['重量'].str.replace(pat=',', repl='')
+    csv['價值'] = csv['價值'].str.replace(pat=',', repl='')
+
+    # add an id to index
+    csv.index.name = '_id'
+
+    ## save the csv file back
+    csv.to_csv(csv_path, sep=',', encoding='utf-8')
+    print('parse_weight_col_to_int(' + csv_path + ') finished!')
+
+file = '2016-08-short'
+txt_path = 'export-goods/' + file + '.txt'
+csv_path = 'export-goods/' + file + '.csv'
+
+
+formalize_to_well_form_csv(txt_path)
+parse_col(csv_path)
+print()
+
+csv = pd.read_csv(csv_path)
+print(csv.dtypes)
+print()
